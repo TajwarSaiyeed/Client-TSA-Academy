61828a163aa5d5e3a8bd4ab870ba1bb200c98843
TajwrSaiyeed <rjabid36@gmail.com>
2022-10-26 13:13:04 +0600

    404 page add

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="relative">
+      <div className="bgImage"></div>
+      <div className=" absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 gap-2 flex">
+        <Link to="/" className="btn btn-primary">
+          Back To Home
+        </Link>
+        <Link to="/courses" className="btn btn-success">
+          Back To Cources
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
