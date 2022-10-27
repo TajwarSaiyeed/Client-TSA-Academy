23f28553d829ab519f99333ac441f70559d6eae2
TajwrSaiyeed <rjabid36@gmail.com>
2022-10-27 09:35:41 +0600

    course card details section responsive done

--- src/components/CourseCardDetails/CourseCardDetails.jsx
+++ src/components/CourseCardDetails/CourseCardDetails.jsx
@@ -8,16 +8,20 @@ const CourseCardDetails = () => {
   const courseDetails = useLoaderData();
   const { _id, name, desc, img } = courseDetails;
   return (
-    <div className="flex justify-center items-center flex-col rounded-lg min-h-screen gap-5">
+    <div className="flex justify-center items-center flex-col rounded-lg min-h-screen gap-5 m-2">
       <div
-        className="hero min-h-2/4 h-96 w-2/4 bg-base-200 rounded-lg"
+        className="hero h-2/4 w-96 lg:w-3/4 lg:h-96  bg-base-200 rounded-lg"
         ref={ref}
       >
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={img} className="max-w-sm rounded-lg shadow-2xl" alt="" />
-          <div>
-            <h1 className="text-5xl font-bold">{name}</h1>
-            <p className="py-6">{desc}</p>
+          <img
+            src={img}
+            className="max-w-sm shadow-2xl p-3 rounded-xl"
+            alt=""
+          />
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl uppercase font-bold">{name}</h1>
+            <p className="py-6 text-justify">{desc}</p>
             <div className="gap-2 flex">
               <Link to={`/checkout/${_id}`}>
                 <button className="btn btn-primary">Get Premium Access</button>
